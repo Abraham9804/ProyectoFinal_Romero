0e45a57e777a46eab84d77943b8f41d21db7e584
Romero <romero9804@hotmail.com>
2022-08-07 20:49:44 -0700

    modificacion de menu y portada

--- script.js
+++ script.js
@@ -1,13 +1,201 @@
-let multiplicando
-let multiplicador
-multiplicando = prompt("ingrese la tabla de multiplicacion deseada")
-multiplicador = prompt("ingrese hasta que numero desea multiplicar")
+//DEFINICION DE VARIABLES
+let cantidad
+let meses
+let total
+let pagos
+let interes
+let insertar
+let prestamos=[]
 
 
-
-
-
-for(let i =1; i <= multiplicador; i ++){
-    operacion = multiplicando * i
-    console.log(multiplicando + ' X ' + i + ' = ' + operacion )
+//DEFINICION DE CLASE
+class Prestamo{
+    constructor(cantidad, meses, total, pagos){
+        this.cantidad=cantidad
+        this.meses=meses
+        this.total=total 
+        this.pagos=pagos
+    }
+    
 }
+
+
+//CONEXION CON API PARA MOSTRAR EL PRECIO DEL DOLAR
+fetch("https://criptoya.com/api/dolar")
+.then(response => response.json())
+.then(({oficial, solidario, blue}) => {		
+    divDollar.innerHTML=`
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Oficial </div>
+        <div class="card-body">
+            <h4 class="card-title">$${oficial}</h4>
+            <p class="card-text"></p>
+        </div>
+    </div>
+
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Solidario </div>
+        <div class="card-body">
+            <h4 class="card-title">$${solidario}</h4>
+            <p class="card-text"></p>
+        </div>
+    </div>
+
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Blue </div>
+        <div class="card-body">
+            <h4 class="card-title">$${blue}</h4>
+            <p class="card-text"></p>
+        </div>
+    </div>
+    
+    `
+  })
+
+//FUNCION PARA ACTUALIZAR EL PRECIO DEL DOLAR DESDE LA API
+  const dolar = document.getElementById('divdolar')
+  setInterval(() => {
+  fetch("https://criptoya.com/api/dolar")
+  .then(response => response.json())
+  .then(({oficial, solidario, blue}) => {		
+      divDollar.innerHTML=`
+      <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Oficial </div>
+        <div class="card-body">
+            <h4 class="card-title">$${oficial}</h4>
+            <p class="card-text"></p>
+        </div>
+    </div>
+
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Solidario </div>
+        <div class="card-body">
+            <h4 class="card-title">$${solidario}</h4>
+            <p class="card-text"></p>
+        </div>
+    </div>
+
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Blue </div>
+        <div class="card-body">
+            <h4 class="card-title">$${blue}</h4>
+            <p class="card-text"></p>
+        </div>
+    </div>
+      `
+    })},350000
+  )
+
+
+
+//VERIFICACION DE COTIZACCIONES EN LOCAL STORAGE
+if(localStorage.getItem('storagePrestamos')){
+    prestamos = JSON.parse(localStorage.getItem('storagePrestamos'))
+} else{
+    localStorage.setItem('storagePrestamos', JSON.stringify(prestamos))
+}
+
+
+//ACCESO A ELEMENTOS DEL COTIZADOR MEDIANTE DOM
+const formulario = document.getElementById('idForm') 
+const botonTareas = document.getElementById('btn-tareas')
+const divPrestamos = document.getElementById('div-prestamos')
+
+
+//FUNCION PARA GUARDAR LA COTIZACION DEL PRESTAMO
+formulario.addEventListener('submit',(event) =>{	
+	event.preventDefault()	
+    cantidad = parseFloat((document.getElementById('idMonto').value))
+	meses = parseFloat((document.getElementById('idMeses').value))
+    
+    totales() //SE LLAMA LA FUNCION PARA CALCULAR PAGOS, PLAZOS E INTERES DEL PRESTAMO
+
+    if((cantidad >=1000 && cantidad <=50000) && (meses >=1 && meses <=12)){
+        const prestamo1 = new Prestamo(cantidad, meses, total, pagos)
+        prestamos.push(prestamo1)
+        localStorage.setItem('storagePrestamos', JSON.stringify(prestamos))
+    }
+
+    formulario.reset()
+    insertarDatos()
+})
+
+
+//FUNCION PARA CALCULAR LOS PAGOS, PLAZOS E INTERES DEL PRESTAMO
+function totales(){
+    if((meses <= 3) && (cantidad >= 1000 && cantidad <=50000)){
+        interes = 0.10
+        total = cantidad + (cantidad * interes)
+        pagos = parseFloat((total / meses).toFixed(2))  
+    }
+
+    else if((meses >3 && meses <= 6) && (cantidad >= 1000 && cantidad <=50000)) {
+        interes = 0.25
+        total = cantidad + (cantidad * interes)
+        pagos = parseFloat((total / meses).toFixed(2))
+    }
+
+    else if((meses >6 && meses <= 12) && (cantidad >= 1000 && cantidad <=50000)) {
+        interes = 0.50
+        total = cantidad + (cantidad * interes)
+        pagos = parseFloat((total / meses).toFixed(2))
+    } 
+}
+
+
+
+//FUNCION PARA VERIFICAR QUE LA INFORMACION INGRESADA AL SIMULADOR ES CORRECTA
+function insertarDatos(){
+    if((meses >=1 && meses <= 12) && (cantidad >= 1000 && cantidad <=50000)){
+        prestamos.forEach(dato => {
+        resultado.innerHTML =`
+        <p>El prestamo solicitado es de: ${dato.cantidad} pesos a pagar en ${dato.meses} meses, 
+        por lo que usted pagará un total de ${dato.total} pesos en cuotas de ${dato.pagos} mensuales</p>`
+})} else if((meses >12) || (cantidad <1000 || cantidad >50000)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Los datos ingresados no son correctos, intentelo nuevamente',
+            confirmButtonColor: '#0d6efd',
+          })
+}
+}
+
+
+//EVENTO PARA MOSTRAR LAS COTIZACIONES GUARDADAS EN LOCALSTORAGE
+botonTareas.addEventListener('click', () =>{
+    let prestamoStorage = JSON.parse(localStorage.getItem('storagePrestamos'))
+    divprestamos.innerHTML=""
+    prestamoStorage.forEach((dato, indice) => {
+        divprestamos.innerHTML+=`
+            <div class="row" id='prestamo${indice}'>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Cotizacion de ${dato.cantidad}$ pesos</h5>
+                            <p>El prestamo solicitado es de: ${dato.cantidad} pesos a pagar en ${dato.meses} meses, 
+                            por lo que usted pagará un total de ${dato.total} pesos en cuotas de ${dato.pagos} mensuales</p>
+                            <a href="#" class="btn btn-danger" id="btn-eliminar">Eliminar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+            <br>
+            `
+        })
+
+//FUNCION PARA ELIMINAR UNA COTIZACION DE LOCALSTORAGE
+    prestamoStorage.forEach((dato,indice) => {
+    document.getElementById(`prestamo${indice}`).lastElementChild.lastElementChild.lastElementChild.lastElementChild.addEventListener("click",()=>{ 
+    document.getElementById(`prestamo${indice}`).remove()
+    prestamos.splice(indice,1)
+    localStorage.setItem('storagePrestamos',JSON.stringify(prestamos))
+    })
+     
+     })
+})
+
+
+
+
